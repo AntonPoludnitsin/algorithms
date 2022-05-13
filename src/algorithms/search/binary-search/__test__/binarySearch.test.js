83661749fc703e70cf8a21b821cabbd494c45e2d
Полудницин Антон В <av.poludnitsin@jet.su>
2022-05-13 12:30:26 +0400

    add algorithm of binary search

---
+++ src/algorithms/search/binary-search/__test__/binarySearch.test.js
@@ -0,0 +1,12 @@
+import { binary_search } from "../binarySearch";
+
+describe("binary_search", () => {
+    it("should return correct value", () => {
+        expect(binary_search([], 5)).toBe(-1);
+        expect(binary_search([1], 1)).toBe(0);
+        expect(binary_search([1, 5], 1)).toBe(0);
+        expect(binary_search([1, 5, 7, 9, 23, 67, 100], 5)).toBe(1);
+        expect(binary_search([1, 5, 7, 9, 23, 67, 100], 67)).toBe(5);
+        expect(binary_search([1, 5, 7, 9, 23, 67, 100], 50)).toBe(-1);
+    })
+})
---
+++ src/algorithms/search/binary-search/binarySearch.js
@@ -0,0 +1,19 @@
+export const binary_search = (array, targetNumber) => {
+    const length = array.length - 1;
+    let lowIndex = 0;
+    let highIndex = length;
+
+    while (lowIndex <= highIndex) {
+        const middleIndex = lowIndex + Math.floor(highIndex - lowIndex);
+
+        if (array[middleIndex] === targetNumber) {
+            return middleIndex;
+        } else if (array[middleIndex] > targetNumber) {
+            highIndex = middleIndex - 1;
+        } else {
+            lowIndex = middleIndex + 1;
+        }
+    }
+
+    return -1;
+}
